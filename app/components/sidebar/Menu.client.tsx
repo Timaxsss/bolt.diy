b44dd6d33f17c4af911ab8e8bf767fe5d3d72f69
Tim. <91551999+Timaxsss@users.noreply.github.com>
2025-01-08 17:51:38 +0100

    Update Menu.client.tsx

--- app/components/sidebar/Menu.client.tsx
+++ app/components/sidebar/Menu.client.tsx
@@ -222,11 +222,9 @@ export const Menu = () => {
           </DialogRoot>
         </div>
         <div className="flex items-center justify-between border-t border-bolt-elements-borderColor p-4">
-          <SettingsButton onClick={() => setIsSettingsOpen(true)} />
           <ThemeSwitch />
         </div>
       </div>
-      <SettingsWindow open={isSettingsOpen} onClose={() => setIsSettingsOpen(false)} />
     </motion.div>
   );
 };
