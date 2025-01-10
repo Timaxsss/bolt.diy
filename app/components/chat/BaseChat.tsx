561131b5f179a5ebe3d3cc22379af3192262e411
Tim. <91551999+Timaxsss@users.noreply.github.com>
2025-01-10 10:36:59 +0100

    Update BaseChat.tsx

--- app/components/chat/BaseChat.tsx
+++ app/components/chat/BaseChat.tsx
@@ -324,10 +324,10 @@ export const BaseChat = React.forwardRef<HTMLDivElement, BaseChatProps>(
             {!chatStarted && (
               <div id="intro" className="mt-[16vh] max-w-chat mx-auto text-center px-4 lg:px-0">
                 <h1 className="text-3xl lg:text-6xl font-bold text-bolt-elements-textPrimary mb-4 animate-fade-in">
-                  NYLAI, Your Coding AI Agent.
+                  OCYX, Your AI Coding Agent.
                 </h1>
                 <p className="text-md lg:text-xl mb-2 text-bolt-elements-textSecondary animate-fade-in animation-delay-200">
-                  CA: 8CmFVyEtQXarXtMbfwvfjCTBEYjSAZ78V1NuY3iVpump
+                  CA: TBD
                 </p>
                 <p className="text-sm lg:text-md text-bolt-elements-textSecondary animate-fade-in animation-delay-300">
                   Put your OWN API key of your choice, or try for free with open router and google gemini flash 2.0 experimental
@@ -523,7 +523,7 @@ export const BaseChat = React.forwardRef<HTMLDivElement, BaseChatProps>(
                         minHeight: TEXTAREA_MIN_HEIGHT,
                         maxHeight: TEXTAREA_MAX_HEIGHT,
                       }}
-                      placeholder="TODO App, A Web marketplace for solana..."
+                      placeholder="Ask OCYX a TODO App, Snake game, Marketplace..."
                       translate="no"
                     />
                     <ClientOnly>
