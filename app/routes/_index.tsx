b88efc765e232d4de72553fbc7f8819478cb6878
Tim. <91551999+Timaxsss@users.noreply.github.com>
2025-01-27 15:41:17 +0100

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,7 +6,7 @@ import { Header } from '~/components/header/Header';
 import BackgroundRays from '~/components/ui/BackgroundRays';
 
 export const meta: MetaFunction = () => {
-  return [{ title: 'Bolt' }, { name: 'description', content: 'Talk with Bolt, an AI assistant from StackBlitz' }];
+  return [{ title: 'FlowLabs' }, { name: 'description', content: 'Talk with FlowLabs, Solana AI Agent Making DeFi Simple for Everyone.' }];
 };
 
 export const loader = () => json({});
