6b7217fe6e84235374e41856c5cdf424cf7e4aaf
Tim. <91551999+Timaxsss@users.noreply.github.com>
2025-01-27 16:19:18 +0100

    Update index.tsx

--- app/components/ui/BackgroundRays/index.tsx
+++ app/components/ui/BackgroundRays/index.tsx
@@ -15,7 +15,7 @@ const BackgroundRays = () => {
 
   return (
     <div className={styles.container}>
-      <spline-viewer url="https://prod.spline.design/MaxNiP5Uwk87Q1pP/scene.splinecode"></spline-viewer>
+      <spline-viewer url="https://prod.spline.design/vemc3bZyefkMwOLI/scene.splinecode"></spline-viewer>
     </div>
   );
 };
